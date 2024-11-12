fb31e53d73026ee090015a2acfce80c0db9c8e82
chamer79 <chamer79@gmail.com>
2024-11-12 17:51:51 -0500

    created index & delete

--- controllers/trips.js
+++ controllers/trips.js
@@ -3,22 +3,43 @@ const express = require("express")
 const router = express.Router() //<- purpose? Connecting express to react?
 
 
-// Create - POST -
+// Create - POST - /trips
 router.post("/", async (req, res) => {
-    res.json({message: "create route"})
+    // res.json({ message: "create route" })
+
     try {
         const createdTrip = await Trip.create(req.body)
         res.status(201).json(createdTrip)
-    }catch (err) {
-        res.status(500)({ err: error.message })
+    }catch (error) {
+        res.status(500).json({ error: error.message })
     }
 })
 
-// Index/Show All
-
+// Index/Show All - GET - /trips
+router.get("/", async (req, res) => {
+    // res.json({ message: "index route" })
+    
+    try {
+        const getAllTrips = await Trip.find()
+        res.status(200).json(getAllTrips)
+    }catch (error) {
+        res.status(500).json({ error: error.message})
+    }
+})
 
 // Delete
-
+router.delete("/:tripId", async (req, res) => {
+    try {
+        // throw new Error("This is a delete test error.")
+        const deletedTrip = await Trip.findByIdAndDelete(req.params.tripId)
+    }catch (error) {
+        const deleteResponse = { error: error.message }
+        if(res.status === 404){ 
+            return res.json(deleteResponse)
+        }
+        res.status(500).json(deleteResponse)
+    }
+})
 
 // Update
 
